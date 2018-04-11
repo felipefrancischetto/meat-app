64f8a01930c59052fac384cdf46aacb473c5b3cc
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-04-11 16:26:42 -0300

    feature: created routes for componentization.
    created routes for componentization.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
+import { ROUTES } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule,
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
