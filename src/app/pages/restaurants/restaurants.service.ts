26631944daf25de149b87ff84c584317685be439
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-04-11 18:42:32 -0300

    chore: created error-handler with operator catch.
    Created error-handler with operator catch.

--- src/app/pages/restaurants/restaurants.service.ts
+++ src/app/pages/restaurants/restaurants.service.ts
@@ -3,10 +3,12 @@ import { Http } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { Restaurant } from './restaurant/restaurant.model';
 
 import { MEAT_API } from './../../app.api';
+import { ErrorHandler } from './../../app.error-handler';
 
 @Injectable()
 export class RestaurantService {
@@ -17,7 +19,8 @@ export class RestaurantService {
 
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
-      .map(res => res.json());
+      .map(res => res.json())
+      .catch(ErrorHandler.handleError);
   }
 
 }
