27e90b0ef0ccaf1d700661bdb08b7dc417250cb5
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-04-25 14:59:36 -0300

    refactor: added reactive forms and validations.

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+
+import { OrderService } from './order.service';
 
 import { RadioOption } from '../../models/radio-option.model';
-import { OrderService } from './order.service';
 import { CartItem } from '../../models/cart-item.model';
 import { Order, OrderItem } from '../../models/order.model';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order',
@@ -12,7 +14,11 @@ import { Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
+  orderForm: FormGroup;
   delivery = 8;
+
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  numberPattern = /^[0-9]*$/;
 
   paymentOptions: RadioOption[] = [
     { label: 'Dinheiro', value: 'MON' },
@@ -22,10 +28,34 @@ export class OrderComponent implements OnInit {
 
   constructor(
     private orderService: OrderService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit() {
+    this.orderForm = this.fb.group({
+      name:              this.fb.control('', [Validators.required, Validators.minLength(5)]),
+      email:             this.fb.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.fb.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address:           this.fb.control('', [Validators.required, Validators.minLength(5)]),
+      number:            this.fb.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress:   this.fb.control(''),
+      paymentOption:     this.fb.control('', Validators.required),
+    }, { validator: OrderComponent.equalsTo });
+  }
+
+  // tslint:disable-next-line:member-ordering
+  static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+    if (!email || !emailConfirmation) {
+      return undefined;
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true };
+    }
+    return undefined;
   }
 
   itemsValue(): number {
